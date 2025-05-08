d0ace6e267fdb76269787ab3bba793f311b311bd
Adryan Pratama <adryanpratama168@gmail.com>
2025-05-08 14:22:05 +0700

    Update splash_screen.dart

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -15,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
     Timer(Duration(seconds: 3), () {
       Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => const Onboarding()));
+          context, MaterialPageRoute(builder: (context) => OnboardingPage()));
     });
   }
 
